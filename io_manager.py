5e64e5601e12498085c3c623e4f0ae11d505e9fc
Syakir <2603220@singaporetech.edu.sg>
2026-09-23 15:29:25 +0800

    intro

---
+++ io_manager.py
@@ -0,0 +1,3 @@
+print("Hi I am your personal health assistant, Calora!")
+print("I can help you track your health and fitness goals.")
+print("Let's get started by setting up your profile.")  #intro
