b5a022be86577b9c5a3108a344f2f4946d6d4866
Syakir <2603220@singaporetech.edu.sg>
2026-09-23 16:45:27 +0800

    half of user profile

--- io_manager.py
+++ io_manager.py
@@ -18,8 +18,10 @@ def get_user_gender():
     gender = input("Please enter your gender (M/F): ")
     if gender == "M":
         print("Great! Hi bro.")
+        return gender
     elif gender == "F":
         print("Great! Hi sis.")
+        return gender
     else:
         print("Invalid input. Please enter M or F for gender.")
         return get_user_gender()  # Recursively call the function until a valid gender is entered            
@@ -44,6 +46,18 @@ def get_user_height():
         print("Invalid input. Please enter a valid height.")
         return get_user_height()  # Recursively call the function until a valid height is entered
 
+def get_activity_level():
+    activity_level = input("Please enter your activity level (sedentary /workout 1-2 times a week/, moderately active /workout 3-4 times a week/, very active /5-6 times a week/): ")  
+    if activity_level == "sedentary":
+        print("cool ", name)
+        return activity_level
+    elif activity_level == "moderately active":
+        print("That's great ", name)
+        return activity_level  
+    elif activity_level == "very active":
+        print("Wow, that's impressive ", name) 
+        return activity_level  
+
 print("Hi I am your personal health assistant, Calora!")
 print("I can help you track your health and fitness goals.")
 print("Let's get started by setting up your profile.")  #intro
@@ -58,5 +72,7 @@ weight_kg = get_user_weight()
 
 height_cm = get_user_height()
 
-user_profile = [name, age, gender, weight_kg, height_cm]  #store user profile in a list
+activity_level = get_activity_level()
+
+user_profile = [name, age, gender, weight_kg, height_cm, activity_level]  #store user profile in a list
 print("Profile setup complete! Here is your profile information: ", user_profile)
