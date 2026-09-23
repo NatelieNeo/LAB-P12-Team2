921fa9ba4c56f6f213c8e34a37e74924e5e3f9d1
Syakir <2603220@singaporetech.edu.sg>
2026-09-23 15:36:36 +0800

    profile setup

--- io_manager.py
+++ io_manager.py
@@ -1,3 +1,8 @@
 print("Hi I am your personal health assistant, Calora!")
 print("I can help you track your health and fitness goals.")
 print("Let's get started by setting up your profile.")  #intro
+#-------Profile Setup-------
+user_name = input("Please enter your name: ")
+print("Hi! ", user_name, "it's nice to meet you!")
+user_age = input("Please enter your age: ")
+print("Great! Nice to know you're", user_age, "years old.")
